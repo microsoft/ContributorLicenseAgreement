fa4cee0e42d63767c2d4a41bcda07eaff3181ef3
Jeff Wilcox <427913+jeffwilcox@users.noreply.github.com>
2024-06-21 09:19:09 -0700

    Delete src directory (#185)
    The source is no longer built from this location.

--- src/ContributorLicenseAgreement.Core/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("ContributorLicenseAgreement.Core.Tests")]
